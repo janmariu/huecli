1b63a7052801c815d6dc472980fadd6e64cc41f5
Jan Marius <jan.marius@bitbucket.no>
2018-11-22 21:48:02 +0100

    default usage message

--- main.js
+++ main.js
@@ -12,6 +12,15 @@ cmd();
 
 async function cmd()
 {
+	if(process.argv.length == 2)
+	{
+		console.log("Example usage:");
+		console.log(`usage: ${process.argv[1]} room`);
+		console.log(`usage: ${process.argv[1]} light`);
+		console.log(`usage: ${process.argv[1]} room <number> action`);
+		console.log(`usage: ${process.argv[1]} light <number> action`);
+	}
+
 	//app room -> list available rooms
 	if(process.argv[2] === 'room' && !process.argv[3])
 	{	
@@ -29,7 +38,8 @@ async function cmd()
 	}
 
 	//app room number on/off -> switch room lights on or off
-	if(process.argv[2] === 'room' && process.argv[3] && (process.argv[4] === 'on' || process.argv[4] === 'off'))
+	if(process.argv[2] === 'room' && process.argv[3] && (process.argv[4] === 'on' 
+		|| process.argv[4] === 'off'))
 	{
 		console.log(`Turning room ${process.argv[3]} ${process.argv[4]}`);
 		await toggleGroup(process.argv[3], _config, process.argv[4] === 'on');		
